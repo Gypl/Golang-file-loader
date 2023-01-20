b8d8707be299340c39d7ed1791137eba1db2b986
lavlo <lavlovandrejj@gmail.com>
2023-01-21 02:17:45 +0400

    creating directory

--- downloader.go
+++ downloader.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type download struct {
@@ -47,21 +48,27 @@ func handleDownloadRequest(response http.ResponseWriter, request *http.Request) 
 	fmt.Fprintf(response, "Download!")
 }
 
-func getFile(downloadRequest download) (err error) {
-	parsedUrl, err := url.Parse(downloadRequest.Location)
+func getFile(downloadRequest download) error {
+	u, err := url.Parse(downloadRequest.Location)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 
-	response, err := http.Get(downloadRequest.Location)
+	response, err := http.Get(strings.Replace(downloadRequest.Location, " ", "%20", -1))
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 	defer response.Body.Close()
 
-	out, err := os.Create(filepath.Base(parsedUrl.Path))
+	save, err := createSaveDirectory(downloadRequest.Title)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	out, err := os.Create(filepath.Join(save, filepath.Base(u.Path)))
 	defer out.Close()
 	_, err = io.Copy(out, response.Body)
 	if err != nil {
@@ -69,6 +76,25 @@ func getFile(downloadRequest download) (err error) {
 		return err
 	}
 	return nil
+}
+
+func createSaveDirectory(title string) (string, error) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	path := filepath.Join(dir, title)
+	_, err = os.Stat(path)
+
+	// creating directory.
+	if err != nil {
+		err = os.Mkdir(path, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return path, nil
 }
 
 func main() {
