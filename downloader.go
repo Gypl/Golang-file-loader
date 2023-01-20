fd654f0c52469d69f82372a050cfe42f38bc4715
lavlo <lavlovandrejj@gmail.com>
2023-01-21 01:43:55 +0400

    getFile

--- downloader.go
+++ downloader.go
@@ -3,9 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
 )
 
 type download struct {
@@ -20,15 +24,51 @@ func status(respose http.ResponseWriter, request *http.Request) {
 func handleDownloadRequest(response http.ResponseWriter, request *http.Request) {
 	var downloadRequest download
 	r, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		http.Error(response, "bad request", 400)
+		log.Println(err)
+		return
+	}
+	defer request.Body.Close()
+
 	err = json.Unmarshal(r, &downloadRequest)
 	if err != nil {
 		http.Error(response, "bad request: "+err.Error(), 400)
-		log.Println(err)
 		return
 	}
 	log.Printf("%#v", downloadRequest)
 
+	err = getFile(downloadRequest)
+	if err != nil {
+		http.Error(response, "internal server error", 500)
+		return
+	}
+
 	fmt.Fprintf(response, "Download!")
+}
+
+func getFile(downloadRequest download) (err error) {
+	parsedUrl, err := url.Parse(downloadRequest.Location)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	response, err := http.Get(downloadRequest.Location)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	out, err := os.Create(filepath.Base(parsedUrl.Path))
+	defer out.Close()
+	_, err = io.Copy(out, response.Body)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
 }
 
 func main() {
